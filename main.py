8a7c010cbe7e07e5ab402b9c7368a6e2d822f11b
Oleg Potkin <olpotkin@gmail.com>
2017-10-31 19:15:47 +0100

    feat: Load pretrained VGG Model into TensorFlow

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
@@ -32,10 +31,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    graph = tf.get_default_graph()
+
+    tf.save_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph 	= tf.get_default_graph()
+    w1 		= graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep 	= graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    w3 		= graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    w4 		= graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    w7 		= graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    
-    return None, None, None, None, None
+    return w1, keep, w3, w4, w7
 tests.test_load_vgg(load_vgg, tf)
 
 
